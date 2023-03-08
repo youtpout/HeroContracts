11776261d0e00933f84d802cfc831c225536d948
Youtpout <EddyBoughioul@hotmail.fr>
2023-03-08 12:23:57 +0100

    add upgrade marketplace to sell from mint

---
+++ contracts/Marketplacev2.sol
@@ -0,0 +1,363 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "./IERC20.sol";
+import "./IERC721.sol";
+import "./IERC1155.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract MarketplaceV2 is
+    Initializable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    ERC1155HolderUpgradeable,
+    ERC721HolderUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant MASTER_ROLE = keccak256("MASTER_ROLE");
+
+    enum ContractType {
+        Erc721,
+        Erc1155
+    }
+
+    enum OrderStatus {
+        Active,
+        Canceled,
+        Sold
+    }
+
+    struct Order {
+        uint256 id;
+        uint256 tokenId;
+        uint256 initialAmount;
+        uint256 currentAmount;
+        uint256 priceByUnit;
+        address seller;
+        address tokenAccepted;
+        address contractAddress;
+        OrderStatus status;
+        ContractType contractType;
+        bool sellByUnit;
+    }
+
+    // data come from safetransfer method
+    struct TransferData {
+        uint256 priceByUnit;
+        address tokenAccepted;
+        address seller;
+        bool sellByUnit;
+    }
+
+    event OrderAdded(uint256 indexed id, address indexed seller, Order order);
+
+    event OrderCanceled(uint256 indexed id);
+
+    event OrderBought(
+        uint256 indexed id,
+        address indexed buyer,
+        uint256 quantity
+    );
+
+    mapping(address => bool) public approvedToken;
+
+    /// @notice A mapping from order id to order data
+    /// @dev This is a static, ever increasing list
+    mapping(uint256 => Order) public orders;
+
+    /// @notice Mapping from owner to a list of owned auctions
+    mapping(address => uint256[]) public ownedOrders;
+
+    /// @notice The amount of orders
+    uint256 public orderCount;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Pausable_init();
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+        __ERC1155Holder_init();
+        __ERC721Holder_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(MASTER_ROLE, msg.sender);
+
+        // address 0 for native token fantom
+        approvedToken[address(0)] = true;
+    }
+
+    function addTokenAccepted(address tokenAddress)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        approvedToken[tokenAddress] = true;
+    }
+
+    function removeTokenAccepted(address tokenAddress)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        approvedToken[tokenAddress] = false;
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function onERC721Received(
+        address,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        _sellNft(from, tokenId, 1, ContractType.Erc721, data);
+        return this.onERC721Received.selector;
+    }
+
+    function onERC1155Received(
+        address,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        _sellNft(from, id, value, ContractType.Erc1155, data);
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        revert("Not implemented");
+        //return this.onERC1155BatchReceived.selector;
+    }
+
+    function _sellNft(
+        address from,
+        uint256 id,
+        uint256 amount,
+        ContractType contractType,
+        bytes memory data
+    ) internal nonReentrant whenNotPaused {
+        TransferData memory info = abi.decode(data, (TransferData));
+        require(approvedToken[info.tokenAccepted], "Invalid token");
+        // address 0 is when we directly mint token and sell it
+        require(from == info.seller || from == address(0), "Invalid seller");
+        orderCount++;
+
+        Order memory orderInfo = Order(
+            orderCount,
+            id,
+            amount,
+            amount,
+            info.priceByUnit,
+            info.seller,
+            info.tokenAccepted,
+            _msgSender(),
+            OrderStatus.Active,
+            contractType,
+            info.sellByUnit
+        );
+        // save order info
+        orders[orderCount] = orderInfo;
+        // list order by seller
+        ownedOrders[info.seller].push(orderCount);
+
+        emit OrderAdded(orderCount, info.seller, orderInfo);
+    }
+
+    function cancelOrder(uint256 orderId) public nonReentrant whenNotPaused {
+        Order storage order = orders[orderId];
+        require(
+            order.status == OrderStatus.Active,
+            "Order is no longer available!"
+        );
+        require(
+            hasRole(MASTER_ROLE, _msgSender()) || order.seller == _msgSender(),
+            "Only the seller can cancel order"
+        );
+        require(order.currentAmount > 0, "Already sold");
+
+        if (order.contractType == ContractType.Erc721) {
+            IERC721(order.contractAddress).safeTransferFrom(
+                address(this),
+                order.seller,
+                order.tokenId
+            );
+        } else if (order.contractType == ContractType.Erc1155) {
+            IERC1155(order.contractAddress).safeTransferFrom(
+                address(this),
+                order.seller,
+                order.tokenId,
+                order.currentAmount,
+                ""
+            );
+        }
+
+        // update order infos
+        order.currentAmount = 0;
+        order.status = OrderStatus.Canceled;
+
+        emit OrderCanceled(orderId);
+    }
+
+    function buyOrder(
+        address buyer,
+        uint256 orderId,
+        uint256 price,
+        uint256 amount
+    ) public payable nonReentrant whenNotPaused {
+        Order storage order = orders[orderId];
+        require(
+            hasRole(MASTER_ROLE, _msgSender()) || buyer == _msgSender(),
+            "Only the buyer can pay"
+        );
+        require(
+            order.status == OrderStatus.Active,
+            "Order is no longer available!"
+        );
+        require(amount > 0, "You can't buy nothing");
+        require(order.currentAmount >= amount, "Not enough quantity");
+        if (!order.sellByUnit) {
+            require(amount == order.currentAmount, "Can't buy by unit");
+        }
+
+        uint256 orderPrice = order.priceByUnit * amount;
+
+        if (order.tokenAccepted == address(0)) {
+            require(orderPrice == msg.value, "Buy price isn't equal to price!");
+        } else {
+            require(orderPrice == price, "Buy price isn't equal to price!");
+        }
+
+        //todo pay part
+        (address taxReceiver, uint256 tax) = IERC2981Upgradeable(
+            order.contractAddress
+        ).royaltyInfo(order.tokenId, orderPrice);
+
+        require(tax <= orderPrice / 5, "Too much tax");
+        uint256 restToPay = orderPrice - tax;
+
+        if (order.tokenAccepted == address(0)) {
+            if (tax > 0) {
+                // paid royalties
+                (bool sentTax, ) = taxReceiver.call{value: tax}("");
+                require(sentTax, "Failed to send tax Ether");
+            }
+            // paid the seller
+            (bool sent, ) = order.seller.call{value: restToPay}("");
+            require(sent, "Failed to send Ether");
+        } else {
+            if (tax > 0) {
+                // paid royalties
+                bool sentTax = IERC20(order.tokenAccepted).transferFrom(
+                    buyer,
+                    taxReceiver,
+                    tax
+                );
+                require(sentTax, "Failed to send tax Erc20");
+            }
+            // paid the seller
+            bool sent = IERC20(order.tokenAccepted).transferFrom(
+                buyer,
+                order.seller,
+                restToPay
+            );
+            require(sent, "Failed to send ERC20");
+        }
+
+        if (order.contractType == ContractType.Erc721) {
+            IERC721(order.contractAddress).safeTransferFrom(
+                address(this),
+                buyer,
+                order.tokenId
+            );
+        } else if (order.contractType == ContractType.Erc1155) {
+            IERC1155(order.contractAddress).safeTransferFrom(
+                address(this),
+                buyer,
+                order.tokenId,
+                amount,
+                ""
+            );
+        }
+
+        // update order infos
+        uint256 currentAmount = order.currentAmount - amount;
+        order.currentAmount = currentAmount;
+        if (order.currentAmount == 0) {
+            order.status = OrderStatus.Sold;
+        }
+
+        emit OrderBought(orderId, _msgSender(), amount);
+    }
+
+    function fetchPageOrders(uint256 cursor, uint256 howMany)
+        public
+        view
+        returns (Order[] memory values, uint256 newCursor)
+    {
+        uint256 length = howMany;
+        if (length > orderCount - cursor) {
+            length = orderCount - cursor;
+        }
+
+        values = new Order[](length);
+        for (uint256 i = 0; i < length; i++) {
+            values[i] = orders[cursor + i];
+        }
+
+        return (values, cursor + length);
+    }
+
+    function fetchPageOwned(
+        address user,
+        uint256 cursor,
+        uint256 howMany
+    ) public view returns (Order[] memory values, uint256 newCursor) {
+        uint256 length = howMany;
+        uint256[] storage owned = ownedOrders[user];
+        if (length > owned.length - cursor) {
+            length = owned.length - cursor;
+        }
+
+        values = new Order[](length);
+        for (uint256 i = 0; i < length; i++) {
+            uint256 index = owned[cursor + i];
+            values[i] = orders[index];
+        }
+
+        return (values, cursor + length);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155ReceiverUpgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- test/Market.ts
+++ test/Market.ts
@@ -2,7 +2,7 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
-import { GameManager, HeroPoints, HeroPotion, HeroSpell, Marketplace } from "../typechain-types";
+import { GameManager, HeroPoints, HeroPotion, HeroSpell, Marketplace, MarketplaceV2 } from "../typechain-types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { PromiseOrValue } from "../typechain-types/common";
 import { BigNumberish, BytesLike } from "ethers";
@@ -11,7 +11,7 @@ describe("Market", function () {
   var tokenContract: HeroPoints,
     itemContract: HeroPotion,
     spellContract: HeroSpell,
-    shopContract: Marketplace,
+    shopContract: Marketplace | MarketplaceV2,
     gameContract: GameManager;
 
   var owner: SignerWithAddress,
@@ -38,22 +38,30 @@ describe("Market", function () {
     shopContract = await upgrades.deployProxy(ContractShop) as Marketplace;
     await shopContract.deployed();
 
+    const BoxV2 = await ethers.getContractFactory("MarketplaceV2");
+    shopContract = await upgrades.upgradeProxy(shopContract.address, BoxV2) as MarketplaceV2;
+    console.log("Marketplace upgraded");
+
     const ContractGame = await ethers.getContractFactory("GameManager");
     gameContract = await upgrades.deployProxy(ContractGame) as GameManager;
     await gameContract.deployed();
 
     let masterRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MASTER_ROLE"));
+    let minterRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
     await shopContract.grantRole(masterRole, gameContract.address);
     await shopContract.addTokenAccepted(tokenContract.address);
 
     await spellContract.addMarketAddress(gameContract.address);
     await spellContract.addMarketAddress(shopContract.address);
+    await spellContract.grantRole(minterRole, gameContract.address);
 
     await itemContract.addMarketAddress(gameContract.address);
     await itemContract.addMarketAddress(shopContract.address);
+    await itemContract.grantRole(minterRole, gameContract.address);
 
     await tokenContract.addMarketAddress(gameContract.address);
     await tokenContract.addMarketAddress(shopContract.address);
+    await tokenContract.grantRole(minterRole, gameContract.address);
   });
 
 
@@ -67,8 +75,8 @@ describe("Market", function () {
 
       // sell for 3 hero points by unit, accept hero point token, authorize buy at unit
       let threeToken = ethers.utils.parseEther("3");
-      let data = [threeToken, tokenContract.address, true];
-      let dataTransfer = ethers.utils.defaultAbiCoder.encode(["uint256", "address", "bool"], data);
+      let data = [threeToken, tokenContract.address, dev2.address, true];
+      let dataTransfer = ethers.utils.defaultAbiCoder.encode(["uint256", "address", "address", "bool"], data);
 
 
       let action: GameManager.ActionStruct = {
@@ -90,6 +98,61 @@ describe("Market", function () {
 
       itemAmount = await itemContract.balanceOf(dev2.address, 1);
       expect(itemAmount).equal(5);
+
+      let order = await shopContract.orders(1);
+      expect(order.currentAmount).equal(5);
+
+      // mint token for the buyer
+      let tenTokens = ethers.utils.parseEther("10");
+      let mintToken = await tokenContract.mint(dev3.address, tenTokens);
+      await mintToken.wait();
+      let balanceToken = await tokenContract.balanceOf(dev3.address);
+      expect(balanceToken).equal(tenTokens);
+
+      let sixTokens = threeToken.mul(2);
+      let buy = await shopContract.buyOrder(dev3.address, order.id, sixTokens, 2);
+      await buy.wait();
+
+      let itemBuyer = await itemContract.balanceOf(dev3.address, 1);
+      expect(itemBuyer).equal(2);
+
+      let balandeDev2 = await tokenContract.balanceOf(dev2.address);
+      let amountSubtax = sixTokens.sub(sixTokens.mul(5).div(100));
+      expect(balandeDev2).equal(amountSubtax);
+
+      let balanceBank = await tokenContract.balanceOf(owner.address);
+      expect(balanceBank).equal(sixTokens.mul(5).div(100));
+    });
+
+  });
+
+  describe("Test direct sell from minting", function () {
+    it("Should create a order", async function () {
+      let itemAmount = await itemContract.balanceOf(dev2.address, 1);
+      expect(itemAmount).equal(0);
+
+      // sell for 3 hero points by unit, accept hero point token, authorize buy at unit
+      let threeToken = ethers.utils.parseEther("3");
+      let data = [threeToken, tokenContract.address, dev2.address, true];
+      let dataTransfer = ethers.utils.defaultAbiCoder.encode(["uint256", "address", "address", "bool"], data);
+
+      // direct mint to the shop with seller infos
+      let action: GameManager.ActionStruct = {
+        actionType: 1,
+        contractType: 2,
+        contractAddress: itemContract.address,
+        recipient: shopContract.address,
+        spender: dev2.address,
+        amount: 5,
+        tokenId: 1,
+        data: dataTransfer
+      }
+
+      console.log("gamecontract", gameContract.address);
+
+      // will create the order
+      let exec = await gameContract.executeActions([action]);
+      await exec.wait();
 
       let order = await shopContract.orders(1);
       expect(order.currentAmount).equal(5);
